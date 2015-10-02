fdad7c09be9130a4361a41073d35626f9c64e424
chweeks <chrisharryweeks@gmail.com>
2015-10-02 13:18:15 +0100

    removed @choice variable

--- RpsWeb.rb
+++ RpsWeb.rb
@@ -38,15 +38,14 @@ class RpsWeb < Sinatra::Base
   get '/result' do
     choice = session[:choice]
     game = Game.new
-    player1 = Human.new
-    player2 = Computer.new
-    player1.chooses(choice.to_sym)
-    player2.rand_choice
-    @choice = player1.hand
-    @comp_choice = player2.hand
-    return erb :winner if game.winner?(player1, player2)
-    return erb :loser if game.loser?(player1, player2)
-    return erb :draw if game.draw?(player1, player2)
+    @player1 = Human.new
+    @player2 = Computer.new
+    @player1.chooses(choice.to_sym)
+    @player2.rand_choice
+
+    return erb :winner if game.winner?(@player1, @player2)
+    return erb :loser if game.loser?(@player1, @player2)
+    return erb :draw if game.draw?(@player1, @player2)
   end
 
   run! if app_file == $PROGRAM_NAME
