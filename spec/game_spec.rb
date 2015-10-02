ccb72b7dc733e9db1e5729230f7e309f2aab36c6
chweeks <chrisharryweeks@gmail.com>
2015-10-02 13:46:59 +0100

    test names updated

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -5,20 +5,29 @@ describe Game do
   let(:goku) {double :goku, hand: :rock}
   let(:krillin) {double :krillin, hand: :spock}
 
-  it 'should return true if human is winner' do
-    vegeta = double :computer, hand: :scissors
-    expect(subject.winner?(goku, vegeta)).to be true
-    expect(subject.winner?(krillin, vegeta)).to be true
+  context('#winner?') do
+
+    it 'should return true if human is winner' do
+      vegeta = double :computer, hand: :scissors
+      expect(subject.winner?(goku, vegeta)).to be true
+      expect(subject.winner?(krillin, vegeta)).to be true
+    end
   end
 
-  it 'should return true if it\'s a draw' do
-    vegeta = double :computer, hand: :rock
-    expect(subject.draw?(goku, vegeta)).to be true
+  context('#winner?') do
+
+    it 'should return true if it\'s a draw' do
+      vegeta = double :computer, hand: :rock
+      expect(subject.draw?(goku, vegeta)).to be true
+    end
   end
 
-  it 'should return true if human loses' do
-    vegeta = double :computer, hand: :paper
-    expect(subject.loser?(goku, vegeta)).to be true
-    expect(subject.loser?(krillin, vegeta)).to be true
+  context('#winner?') do
+
+    it 'should return true if human loses' do
+      vegeta = double :computer, hand: :paper
+      expect(subject.loser?(goku, vegeta)).to be true
+      expect(subject.loser?(krillin, vegeta)).to be true
+    end
   end
 end
